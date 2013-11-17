a77702dad1c641484b891715e367032cc57303e9
rachem <rachem.goruchon@ingesup.com>
2013-11-17 16:33:36 +0100

    commit 2

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User
+  include Mongoid::Document
+  field :provider, type: String
+  field :uid, type: String
+  field :name, type: String
+  field :email, type: String
+  field :updtates, type: String
+  attr_accessible :provider, :uid, :name, :email, :updates
+  
+  # run 'rake db:mongoid:create_indexes' to create indexes
+  index({ email: 1 }, { unique: true, background: true })
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+         user.name = auth['info']['name'] || ""
+         user.email = auth['info']['email'] || ""
+         user.updates = auth['info']['updates'] || ""
+      end
+    end
+  end
+
+end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :buffer,'5284e3af47cd66761d000153', 'b2e3765572ec32baaccf516671c7caa7'
+end
